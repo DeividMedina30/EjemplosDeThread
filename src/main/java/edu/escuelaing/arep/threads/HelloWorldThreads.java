b1b813778c8f40ab2a108d5ee9a73abe421ea0c2
unknown <deivid.medina@labinfo.is.escuelaing.edu.co>
2022-03-01 15:29:01 -0500

    Creando Hilos en paralelo.

--- src/main/java/edu/escuelaing/arep/threads/HelloWorldThreads.java
+++ src/main/java/edu/escuelaing/arep/threads/HelloWorldThreads.java
@@ -1,5 +1,14 @@
 package edu.escuelaing.arep.threads;
 
-public class HelloWorldThreads {
-    
+public class HelloWorldThreads extends Thread{
+
+    @Override
+    public void run(){
+        System.out.println("Hello from run: " + this.toString());
+    }
+
+    public static void main(String[] args){
+        (new HelloWorldThreads()).start();
+        System.out.println("Hello from main: " + (Thread.currentThread().toString()));
+    }
 }
